1d6e710b21a17fcd9723f3889f15f41dc966cde5
Anthony <anthony@anthony-HP-Pavilion-dv6700-Notebook-PC.(none)>
2012-10-20 18:56:04 -0400

    Sdfsdf

--- udpserv.c
+++ udpserv.c
@@ -75,7 +75,7 @@ int main(int argc, char **argv)
 		bzero( &sa, sizeof( sa ) );
 	
 		sa->sin_family = AF_INET;
-		printf("%s\n",configdata[0].data )
+		printf("%s\n",configdata[0].data );
 //		sa->sin_port = htons( configdata[0].data );
 		bind( sockfd[sockcount], (SA *) sa, sizeof( *sa ) );
 		printf( "bound---\n" );	
