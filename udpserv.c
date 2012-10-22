157a9d71eb6fa64ef39bc6deb624e27602196ea5
Anthony <anthony@anthony-HP-Pavilion-dv6700-Notebook-PC.(none)>
2012-10-22 03:13:05 -0400

    sdfs

--- udpserv.c
+++ udpserv.c
@@ -78,7 +78,8 @@ int main(int argc, char **argv)
 
 		sa->sin_family = AF_INET;
 		sa->sin_port = htons( ( size_t )configdata[0].data );
-		printf( "BOUND SOCKET BIND RETURN VALUE : %d\n", bind( sockfd[sockcount], (SA *) sa, sizeof( *sa ) ) );
+		int b = bind( sockfd[sockcount], (SA *) sa, sizeof( *sa ) );
+		printf( "BOUND SOCKET BIND RETURN VALUE : %d\n", b );
 		sockinfo[ sockcount ].sockfd = sockfd[ sockcount ];		
 		sockinfo[ sockcount ].ip_addr = (SA *)sa;
 		sockinfo[ sockcount ].ntmaddr = (struct sockaddr_in *)ifi->ifi_ntmaddr;
