92a1465ceae5663c0d690019f9229a5db019e6fa
Anthony <anthony@anthony-HP-Pavilion-dv6700-Notebook-PC.(none)>
2012-10-22 05:15:33 -0400

    sdfs

--- udpcli.c
+++ udpcli.c
@@ -238,7 +238,7 @@ int main( int argc, char **argv )
 	servaddr.sin_port = htonl(12345); //assigning server port from client.in;
 	inet_pton( AF_INET, IPServer, &servaddr.sin_addr );
 	printf(" calling dg_cli\n");	
-    	dg_cli( stdin, sockfd1, (SA *)&servaddr, sizeof( servaddr ), configdata );
+    	dg_cli1( stdin, sockfd1, (SA *)&servaddr, sizeof( servaddr ), configdata );
 	exit(0);
 }
 
