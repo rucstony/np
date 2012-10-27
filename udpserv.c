0069228202451c0f01f2a13ef996b2f46f6b9e3d
Anthony <anthony@anthony-HP-Pavilion-dv6700-Notebook-PC.(none)>
2012-10-26 21:56:32 -0400

    Ssdfs

--- udpserv.c
+++ udpserv.c
@@ -349,8 +349,8 @@ void mydg_echo( int sockfd, SA *servaddr, socklen_t servlen, SA *cliaddr , sockl
 		/* Pick the data from the file  */ 
 		printf("Calling dg_send() with picked up data..\n");
 
-	printf("dg_send(): Destination Address : %s\n", inet_ntop( AF_INET, cliaddr->sin_addr, IPClient, MAXLINE ));
-	printf( "dg_send(): Destination Port: %d\n", cliaddr->sin_port );
+	printf("dg_send(): Destination Address : %s\n", inet_ntop( AF_INET, *(cliaddr).sin_addr, IPClient, MAXLINE ));
+	printf( "dg_send(): Destination Port: %d\n", *(cliaddr).sin_port );
 
 	//	send( connfd, sendline, strlen( sendline ), 0 );
 
