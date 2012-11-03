dce2f8a150589d65f03872e6f92efb73771d14f9
Anthony <anthony@anthony-HP-Pavilion-dv6700-Notebook-PC.(none)>
2012-11-03 07:18:13 -0400

    fgdfg

--- udpserv.c
+++ udpserv.c
@@ -503,7 +503,7 @@ void mydg_echo( int sockfd, SA *servaddr, socklen_t servlen, SA *cliaddr , sockl
 	}
 	
 //	setsockopt( sockinfo[ i ].sockfd, SOL_SOCKET, SO_DONTROUTE, &on, sizeof( on ) );  
-	printf("Setting SO_DONTROUTE to %d..\n", getsockopt( sockfd, SOL_SOCKET, SO_DONTROUTE, &on, &onlength ) );
+	printf("Setting SO_DONTROUTE to %d..\n", getsockopt( sockfd, SOL_SOCKET, SO_DONTROUTE, &on, onlength ) );
 	perror( "getsockopt" );
 	setsockopt( connfd, SOL_SOCKET, SO_DONTROUTE, &on, sizeof( on ) );
 
