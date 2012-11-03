aab5ada2d1221801eefb31139f5cf4c539eda131
Anthony <anthony@anthony-HP-Pavilion-dv6700-Notebook-PC.(none)>
2012-11-03 06:49:51 -0400

    fgdfg

--- udpserv.c
+++ udpserv.c
@@ -506,7 +506,7 @@ void mydg_echo( int sockfd, SA *servaddr, socklen_t servlen, SA *cliaddr , sockl
 	getsockopt( sockfd, SOL_SOCKET, SO_DONTROUTE, &on, sizeof( on ) );
 	setsockopt( connfd, SOL_SOCKET, SO_DONTROUTE, &on, sizeof( on ) );
 
-	printf("Setting SO_DONTROUTE to %d..\n", on );
+	printf("Setting SO_DONTROUTE to %d..\n", getsockopt( sockfd, SOL_SOCKET, SO_DONTROUTE, &on, sizeof( on ) ) );
 	/* Bind to IPServer and EPHEMERAL PORT and return EPHEMERAL PORT */
 
 	bind( connfd, (SA *) servaddr, sizeof( struct sockaddr_in ) );
