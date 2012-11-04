b352b2441273c4f90bf0c03de276acfa6a6dc622
Anthony <anthony@anthony-HP-Pavilion-dv6700-Notebook-PC.(none)>
2012-11-04 15:02:59 -0500

    Sdfsd

--- rtt_plus.c
+++ rtt_plus.c
@@ -71,7 +71,7 @@ rtt_start(struct rtt_info *ptr)
     	value.it_interval.tv_usec = 0;  /* Two hundred milliseconds */
     	value.it_value.tv_sec = 0;           /* Zero seconds */
     	value.it_value.tv_usec = ptr->rtt_rto*1000;     /* Five hundred milliseconds */
-	printf(" \n rtt_start(): ********** value.it_value.tv_usec : %d **************\n",value.it_value.tv_usec);
+	printf(" \n I AM PRINTING THIS rtt_start(): ********** value.it_value.tv_usec : %d **************\n",value.it_value.tv_usec);
 	
 	return(value);		/* round float to int */
 		/* 4return value can be used as: alarm(rtt_start(&foo)) */
